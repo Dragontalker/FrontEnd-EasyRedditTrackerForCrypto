5b4ed4fc2fa65a0c4a73af636ad4a27cab17c7d1
Richard Yang <richard.yang.tong@gmail.com>
2021-02-02 14:33:22 -0500

    Commented out the function that is not working.

--- demoAPI/redditAPI/reddit.js
+++ demoAPI/redditAPI/reddit.js
@@ -11,23 +11,26 @@ searchForm.addEventListener('submit', event => {
     //Check input
     if (searchTerm === '') {
         // Show message
-        showMessage('Please add a serach term', 'alert-danger')
+        alert('Please add a serach term')
     }
     event.preventDefault();
 });
 
-function showMessage(message, className) {
-    // Create div
-    const div = document.createElement('div');
-    // Add classes
-    div.className = `alert ${className}`;
-    // Add text
-    div.appendChild(document.createTextNode(message));
-    // Get parent container
-    const searchContainer = document.getElementById('search-container');
-    // Get search
-    const search = document.getElementById('search');
+// function showMessage(message, className) {
+//     // Create div
+//     const div = document.createElement('div');
+//     // Add classes
+//     div.className = `alert ${className}`;
+//     // Add text
+//     div.appendChild(document.createTextNode(message));
+//     // Get parent container
+//     const searchContainer = document.getElementById('search-container');
+//     // Get search
+//     const search = document.getElementById('search');
 
-    // Inseert message
-    searchContainer.insertBefore(div, search);
-}
+//     // Inseert message
+//     searchContainer.insertBefore(div, search);
+
+//     // Timeout alert
+//     setTimeout(() => {document.querySelector('alert').remove()}, 1000);
+// }
