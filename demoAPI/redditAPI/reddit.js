933835c2108ba64c07b95611f5baf585cdb4d00b
Richard Yang <richard.yang.tong@gmail.com>
2021-02-02 14:11:31 -0500

    Attached an event listener to submit button.

---
+++ demoAPI/redditAPI/reddit.js
@@ -0,0 +1,7 @@
+const searchForm = document.getElementById('search-form');
+const serachInput = document.getElementById('search-input');
+
+searchForm.addEventListener('submit', event => {
+    console.log(123)
+    event.preventDefault();
+})
