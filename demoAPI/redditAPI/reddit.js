6248e6fc9267db6202e37fd69e67cb3d827ca20c
Richard Yang <richard.yang.tong@gmail.com>
2021-02-02 14:17:42 -0500

    Added a new function for submit button.

--- demoAPI/redditAPI/reddit.js
+++ demoAPI/redditAPI/reddit.js
@@ -2,6 +2,11 @@ const searchForm = document.getElementById('search-form');
 const serachInput = document.getElementById('search-input');
 
 searchForm.addEventListener('submit', event => {
-    console.log(123)
+    // Get search term by toggling on/off
+    const searchTerm = searchInput.value;
+    
+    // Get sort, use special psudo-selector 
+    const sortBy = document.querySelector('input[name="sortby"]:checked').value;
+    console.log(sortBy);
     event.preventDefault();
 })
