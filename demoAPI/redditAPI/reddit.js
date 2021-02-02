25824a84db744f2e613ba14d9ec35d731d07eec8
Richard Yang <richard.yang.tong@gmail.com>
2021-02-02 14:54:54 -0500

    Added a catch to the fetch call.

--- demoAPI/redditAPI/reddit.js
+++ demoAPI/redditAPI/reddit.js
@@ -6,7 +6,8 @@ const searchInput = document.getElementById('search-input');
 const searchReddit = (searchTerm, searchLimit, sortBy) => {
     return fetch(`http://www.reddit.com/search.json?q=${searchTerm}&sort=${sortBy}&limit=${searchLimit}`)
     .then(response => response.json())
-    .then(data => data.data.children.map(data => data.data));
+    .then(data => data.data.children.map(data => data.data))
+    .catch(err => console.log(err));
 };
 
 searchForm.addEventListener('submit', event => {
--- reddit-api.js
+++ reddit-api.js
@@ -1,7 +1,8 @@
 const searchReddit = (searchTerm, searchLimit, sortBy) => {
     return fetch(`http://www.reddit.com/search.json?q=${searchTerm}&sort=${sortBy}&limit=${searchLimit}`)
     .then(response => response.json())
-    .then(data => data.data.children.map(data => data.data));
+    .then(data => data.data.children.map(data => data.data))
+    .catch(err => console.log(err));
 };
 
 // Test
